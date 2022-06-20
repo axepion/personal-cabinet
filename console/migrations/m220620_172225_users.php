ad7f75fe1d8c49c150703a2135c378e59ef3b452
vadimizer <axepion7250@gmail.com>
2022-06-20 20:30:58 +0300

    renew db

--- console/migrations/m220620_172225_users.php
+++ console/migrations/m220620_172225_users.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m220620_033827_users
+ * Class m220620_172225_users
  */
-class m220620_033827_users extends Migration
+class m220620_172225_users extends Migration
 {
     /**
      * {@inheritdoc}
@@ -20,7 +20,7 @@ class m220620_033827_users extends Migration
      */
     public function safeDown()
     {
-        echo "m220620_033827_users cannot be reverted.\n";
+        echo "m220620_172225_users cannot be reverted.\n";
 
         return false;
     }
@@ -33,15 +33,20 @@ class m220620_033827_users extends Migration
             'id' => $this->primaryKey(),
             'login' => $this->string(300),
             'password' => $this->string(300),
-        ]);
-
+            'firstName' => $this->string(300),
+            'middleName' => $this->string(300),
+            'lastName' => $this->string(300),
+            'dateBirthday' => $this->date(),
+            'about' => $this->string(),
+            'photo' => $this->string(),
+            ]);
     }
-    /*
+
     public function down()
     {
-        echo "m220620_033827_users cannot be reverted.\n";
+        echo "m220620_172225_users cannot be reverted.\n";
 
         return false;
     }
-    */
+
 }
--- frontend/views/users/index.php
+++ frontend/views/users/index.php
@@ -9,6 +9,13 @@ $this->title='Users';
     <?php foreach ($users as $user): ?>
     <li class="list-group-item">
         <?= Html::encode("$user->login") ?>
+        <?= Html::encode("$user->firstName") ?>
+        <?= Html::encode("$user->middleName") ?>
+        <?= Html::encode("$user->lastName") ?>
+        <?= Html::encode("$user->dateBirthday") ?>
+        <?= Html::encode("$user->photo") ?>
+        <?= Html::encode("$user->about") ?>
+        <?= Html::encode("$user->photo") ?>
     </li>
     <?php endforeach; ?>
 </ul>
